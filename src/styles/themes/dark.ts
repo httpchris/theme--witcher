1192a6fdc8537ae5cd09b73619852a2d5c807b3f
httpchris <guilherme.costa@bandtec.com.br>
2020-08-24 23:18:11 -0300

    Criação da theme switcher

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,12 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#090809',
+    secundary: '#F7F9F9',
+
+    backgrond: '#222',
+    text: '#FFF',
+    iceBlue: '#BCF8EC'
+  },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,12 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#090809',
+    secundary: '#F7F9F9',
+
+    backgrond: '#F5F5F5',
+    text: '#333',
+    iceBlue: '#BCF8EC'
+  },
+};
